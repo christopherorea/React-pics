0b75fba56f64beb132e48a20590ddf9ce114484c
christopherorea <c28_09_98@hotmail.com>
2020-08-05 14:00:10 -0500

    Updates

--- src/API/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-	baseURL: 'https://api.unsplash.com',
-	headers:{
-		Authorization: 'Client-ID FfAVwrDySRq7stH8Sq0YQlxhxs3cR2IwHY0I49uQI3Q',
-	} 
-});
